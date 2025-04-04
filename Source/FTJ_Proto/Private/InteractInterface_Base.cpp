b3221b464c9bc2fad3108b6ab259cdaadbb4436f
Ewan-DuplessisK <148007352+Ewan-DuplessisK@users.noreply.github.com>
2025-04-04 15:03:11 +0200

    [Dev] Base C++ Classes

---
+++ Source/FTJ_Proto/Private/InteractInterface_Base.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InteractInterface_Base.h"
+
+InteractInterface_Base::InteractInterface_Base()
+{
+}
+
+InteractInterface_Base::~InteractInterface_Base()
+{
+}
