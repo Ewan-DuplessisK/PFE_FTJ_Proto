9a652a5da6fead20cd4f6acc8818f9f81af88050
MERKYRIE <yablokovandrew2003@gmail.com>
2025-04-09 14:25:21 +0200

    [Chore] : Optimization

--- Source/FTJ_Proto/Private/Destruction/FTJ_ProtoDestructionActor.cpp
+++ Source/FTJ_Proto/Private/Destruction/FTJ_ProtoDestructionActor.cpp
@@ -6,8 +6,6 @@ void AFTJ_ProtoDestructionActor::BeginPlay()
 {
     //Logic in Actor
     Super::BeginPlay();
-    //The entire world
-    FBox Box{FVector{-1'000'000'000} , FVector{+1'000'000'000}};
     //Makes Geometry Collection static automatically from the very beginning to avoid invalid collision with entities
-    GeometryCollectionComponent->SetAnchoredByBox(Box , true);
+    GeometryCollectionComponent->SetAnchoredByIndex(0 , true);
 }
